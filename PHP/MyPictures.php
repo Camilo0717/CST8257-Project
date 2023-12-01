07605f9ad184d16546b56db8f7a2127c8d37be3f
Miguel <loui0064@algonquinlive.com>
2023-12-01 12:11:43 -0500

    completed code Addalbum/MyPictures/UploadPictures -need to update style

--- PHP/MyPictures.php
+++ PHP/MyPictures.php
@@ -18,30 +18,37 @@ if (!$isLogged) {
 // Get the current user's ID from the session
 $currentUserId = $_SESSION['userId'];
 
-$selectedAlbumId = $_POST['albumSelection'] ?? null;
-$thumbnails = [];
+// Check if album is selected
+$selectedAlbumId = $_GET['albumSelection'] ?? null;
+// Check if picture is selected
 $selectedPictureId = $_GET['selectedPicture'] ?? null;
+
+// Check for POST request and add comment
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['commentText']) && $selectedPictureId) {
+    // Add a new comment
+    addComment($selectedPictureId, $currentUserId, $_POST['commentText']);
+    // Reload the page to show new comments for the same picture
+    header("Location: MyPictures.php?selectedPicture=$selectedPictureId&albumSelection=$selectedAlbumId");
+    exit;
+}
+
+$thumbnails = [];
 $selectedPictureDetails = null;
 $pictureComments = []; // Initialize as an empty array
-// Debugging: Echo the selected album ID and picture ID
-echo "Selected Album ID: " . htmlspecialchars($selectedAlbumId) . "<br>";
-echo "Selected Picture ID: " . htmlspecialchars($selectedPictureId) . "<br>";
 
 if ($selectedAlbumId) {
     $thumbnails = getThumbnails($selectedAlbumId);
-    echo "Thumbnails count: " . count($thumbnails) . "<br>"; // Check how many thumbnails were retrieved
+    // If no picture is selected, default to the first picture of the album
     if (!$selectedPictureId && !empty($thumbnails)) {
-        $selectedPictureId = $thumbnails[0]['Picture_Id']; // Set the first picture as default
+        $selectedPictureId = $thumbnails[0]['Picture_Id'];
     }
 }
 
+// Fetch details for the selected picture
 if ($selectedPictureId) {
     $selectedPictureDetails = getPictureDetails($selectedPictureId);
-    if (!$selectedPictureDetails) {
-        echo "No details found for Picture ID: " . htmlspecialchars($selectedPictureId) . "<br>";
-    }
+    $pictureComments = getPictureComments($selectedPictureId);
 }
-
 
 include 'Common/PageElements/header.php';
 ?>
@@ -49,48 +56,59 @@ include 'Common/PageElements/header.php';
 <body>
     <div class="container mt-5">
         <h2>Manage My Pictures</h2>
+        <?php
+        if ($selectedAlbumId) {
+            $currentAlbumDetails = getAlbumDetails($selectedAlbumId);
+            echo "<h3>Album: " . htmlspecialchars($currentAlbumDetails['Title']) . "</h3>";
+        }
+        ?>
 
-        <form method="post" id="albumForm">
+        <form method="get" id="albumForm" action="MyPictures.php" class="mb-5">
             <div class="form-group">
-                <label for="albumSelection">Select an Album</label>
-                <?php echo renderAlbumDropdown($currentUserId); ?>
+                <label for="albumSelection">Select an Album:</label>
+                <?php echo renderAlbumDropdown($currentUserId, $selectedAlbumId); ?>
                 <button type="submit" class="btn btn-primary mt-2">Show Pictures</button>
             </div>
         </form>
 
         <!-- Picture Area -->
-        <div id="pictureArea" class="mb-3">
+        <div id="pictureArea" class="mb-5" style="text-align: left;">
             <?php if ($selectedPictureDetails): ?>
-                <img src="image_serve.php?file=<?php echo urlencode($selectedPictureDetails['File_Name']); ?>" class="img-fluid" />
-                <p><?php echo htmlspecialchars($selectedPictureDetails['Description']); ?></p>
+                <img src="image_serve.php?file=<?php echo urlencode($selectedPictureDetails['File_Name']); ?>" class="img-fluid" style="max-width: 900px;" />
+                <?php if (!empty($selectedPictureDetails['Description'])): ?>
+                    <h4 class="mt-3">Description</h4>
+                    <p><?php echo htmlspecialchars($selectedPictureDetails['Description']); ?></p>
+                <?php endif; ?>
             <?php endif; ?>
         </div>
 
         <!-- Thumbnail Bar -->
-        <div class="thumbnail-bar d-flex flex-row">
+        <div class="thumbnail-bar d-flex justify-content-start flex-wrap">
             <?php foreach ($thumbnails as $thumbnail): ?>
-                <a href="MyPictures.php?selectedPicture=<?php echo $thumbnail['Picture_Id']; ?>&albumSelection=<?php echo $selectedAlbumId; ?>">
-                    <img src="image_serve.php?file=<?php echo urlencode($thumbnail['File_Name']); ?>" 
-                         class="img-thumbnail <?php echo $selectedPictureId == $thumbnail['Picture_Id'] ? 'border border-primary' : ''; ?>" />
-                </a>
+                <div class="col-3 mb-5"> 
+                    <a href="MyPictures.php?selectedPicture=<?php echo $thumbnail['Picture_Id']; ?>&albumSelection=<?php echo $selectedAlbumId; ?>">
+                        <img src="image_serve.php?file=<?php echo urlencode($thumbnail['File_Name']); ?>" class="img-fluid" style="width: 100%; height: 225px;" />
+                    </a>
+                </div>
             <?php endforeach; ?>
         </div>
 
-
-
         <!-- Description and Comment Area -->
-        <div id="descriptionAndComments" class="mt-3">
-            <?php if (!empty($pictureComments)): ?>
-                <!-- Loop through and display comments -->
-            <?php else: ?>
-                <p>No comments available.</p>
-            <?php endif; ?>
-        </div>
-
+        <?php if (!empty($pictureComments)): ?>
+            <div id="descriptionAndComments" class="mb-4">
+                <h4>Comments</h4>
+                <?php foreach ($pictureComments as $comment): ?>
+                    <div class="comment mb-3">
+                        <p><strong><?php echo htmlspecialchars($comment['User_Id']); ?>:</strong> <?php echo htmlspecialchars($comment['Comment_Text']); ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Comment Form -->
-        <form id="commentForm" method="post" action="comment_handler.php">
-            <div class="form-group">
+        <form id="commentForm" method="post" action="MyPictures.php?selectedPicture=<?php echo $selectedPictureId; ?>&albumSelection=<?php echo $selectedAlbumId; ?>">
+            <input type="hidden" name="selectedPicture" value="<?php echo htmlspecialchars($selectedPictureId); ?>">
+            <div class="form-group mb-4">
                 <label for="commentText">Leave a Comment:</label>
                 <textarea class="form-control" id="commentText" name="commentText" rows="3"></textarea>
             </div>
@@ -98,12 +116,8 @@ include 'Common/PageElements/header.php';
         </form>
     </div>
 
-
-    <script>
-        // You can add JavaScript if needed for additional interactivity
-    </script>
+    <?php include 'Common/PageElements/Footer.php'; ?>
 </body>
-
 
 <?php
 include 'Common/PageElements/Footer.php';
